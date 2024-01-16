e5c5eca3b36c243ca3c102e92f9abd762dc4f1ac
LollyGolly <valentina_96@inbox.ru>
2024-01-16 23:07:12 +0500

    Tests for constructor SavingAccount were added

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class SavingAccountTest {
 
     @Test
-    public void shouldAddLessThanMaxBalance() {
+    public void shouldAddLessThanMaxBalance() { // -1. Initial Test (функция пополнения счета + баланс в пределах от мин до макс)
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -18,4 +18,113 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
-}
+
+
+    // Проверяем параметры конструктора, условия и исключения по параметрам
+    @Test
+    public void shouldThrowExceptionWhenBalanceLessThanMin() { // -2. Должна выкидываться ошибка, т.к. баланс меньше минимального
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    100,
+                    1_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+    @Test
+    public void shouldNonThrowExceptionWhenBalanceEqualsMin() { // +3. НЕ должна выкидываться ошибка, т.к. баланс может быть равен минимальному значению
+
+        SavingAccount account = new SavingAccount(
+                    1_000,
+                    1_000,
+                    10_000,
+                    5
+            );
+
+        Assertions.assertEquals(account.getBalance(), account.getMinBalance());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenBalanceMoreThanMax() { // -4. Должна выкидываться ошибка, т.к. баланс больше максимального
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    20_000,
+                    1_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+    @Test
+    public void shouldNotThrowExceptionWhenBalanceEqualsMax() { // +5. НЕ должна выкидываться ошибка, т.к. баланс может быть равен максимальному значению
+
+        SavingAccount account = new SavingAccount(
+                10_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(account.getBalance(), account.getMaxBalance());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenMinMoreThanMax() { // -6. Должна выкидываться ошибка, т.к. минимальное значение больше максимального
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    1_000,
+                    10_000,
+                    1_000,
+                    5
+            );
+        });
+    }
+
+    @Test
+    public void shouldNotThrowExceptionWhenMaxEqualsMin() { // +6. НЕ должна выкидываться ошибка, т.к. минимальное значение может равняться максимальному и балансу
+
+        SavingAccount account = new SavingAccount(
+                    10_000,
+                    10_000,
+                    10_000,
+                    5
+            );
+        Assertions.assertEquals(account.getMinBalance(), account.getMaxBalance());
+
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenBalanceIsNegative() { // -7. Должна выкидываться ошибка, т.к. баланс отрицательный
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    -1_000,
+                    1_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+        @Test
+        public void shouldThrowExceptionBecauseRateIsNegative() { // +8. Должна выкидываться ошибка, т.к. ставка отрицательная
+
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                SavingAccount account = new SavingAccount(
+                        1_000,
+                        1_000,
+                        10_000,
+                        -3
+                );
+            });
+        }
+
+
+    }
+
