0754cb8b82b43310278c2a99690ae33ec427e65b
Neto test <akmalova.elza@yandex.ru>
2024-01-16 14:29:08 +0300

    Tests added

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
 
+// тесты на пополнение баланса
+
     @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
@@ -16,5 +18,155 @@ public class CreditAccountTest {
         account.add(3_000);
 
         Assertions.assertEquals(3_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldNotAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                10,
+                5_000,
+                15
+        );
+
+        account.add(-1_000);
+
+        Assertions.assertEquals(10, account.getBalance());
+    }
+
+    @Test
+    public void shouldNotChangeBalance() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    // тесты на оплату
+
+    @Test
+    public void testPositivePay() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        account.pay(1_000);
+
+        Assertions.assertEquals(1_000, account.getBalance());
+    }
+
+    @Test
+    public void testNegativePay() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        account.pay(-1_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void testZeroPay() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        account.pay(0);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void testWhenOverLimit() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        account.pay(6_000);
+
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+
+    // тесты на годовую ставку
+
+    @Test
+    public void testRateWhenPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void testRateWhenNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -2_000,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(-300, account.yearChange());
+    }
+
+    @Test
+    public void testRateWhenZeroBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void testWhenZeroRate() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                0
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+
+    // тесты на кредитный лимит
+    
+
+
+    // тесты исключений
+
+    @Test
+    public void testIllegalArgumentException () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(2_000,5_000,-20));
+    }
+    @Test
+    public void testNegativeCreditLimitException () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(2_000,-5_000,20));
+    }
+    @Test
+    public void testNegativeInitialBalance () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(-2_000,5_000,20));
     }
 }
